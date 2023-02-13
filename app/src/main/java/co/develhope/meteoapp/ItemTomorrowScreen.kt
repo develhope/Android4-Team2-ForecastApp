3e4f7c24a00c7bb3916c5333cad16cd1bc341647
Giuseppe Iacono <giuseiacono10@gmail.com>
2023-02-13 17:10:45 +0100

    latest version + gradle updated

--- app/src/main/java/co/develhope/meteoapp/ItemTomorrowScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/ItemTomorrowScreen.kt
@@ -1,4 +1,13 @@
 package co.develhope.meteoapp
 
-class ItemTomorrowScreen {
-}
+class ItemTomorrowScreen(
+    val grades: Int,
+    val precipitation: Int,
+    val humidity: Int,
+    val UV_Index: Int,
+    val wind: Int,
+    val perc_temp: Int,
+    val coverage: Int,
+    val day: String,
+    val city: String
+)
