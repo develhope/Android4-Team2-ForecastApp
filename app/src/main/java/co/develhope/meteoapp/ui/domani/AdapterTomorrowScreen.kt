6f6aeaf5867a8f687e39b38f799abf937caecc64
Giuseppe Iacono <giuseiacono10@gmail.com>
2023-02-15 15:15:36 +0100

    card done

--- app/src/main/java/co/develhope/meteoapp/ui/domani/AdapterTomorrowScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/domani/AdapterTomorrowScreen.kt
@@ -1,6 +1,34 @@
 package co.develhope.meteoapp.ui.domani
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.databinding.TomorrowScreenRowCardBinding
+
 class AdapterTomorrowScreen (
 
     var items: List<TomorrowCardInfo>
-)
+) : RecyclerView.Adapter<AdapterTomorrowScreen.ItemViewHolder>() {
+
+    inner class ItemViewHolder(val binding: TomorrowScreenRowCardBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = TomorrowScreenRowCardBinding.inflate(layoutInflater, parent, false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.binding.apply {
+            tomorrowHourTV.text = items[position].date
+            tomorrowTempTV.text = items[position].temperature.toString()
+            tomorrowPrecipitationTV.text = items[position].precipitation.toString()
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
--- app/src/main/java/co/develhope/meteoapp/ui/domani/TomorrowCardInfo.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/domani/TomorrowCardInfo.kt
@@ -1,17 +1,16 @@
 package co.develhope.meteoapp.ui.domani
 
 import co.develhope.meteoapp.Weather
-import java.time.OffsetDateTime
 
 data class TomorrowCardInfo(
-    val date: OffsetDateTime,
-    val weather: Weather,
-    val temperature: Int,
-    val precipitation: Int,
-    val perc_temperature: Int,
+    val date: String,
+    //val weather: Weather,
+    val temperature: String,
+    val precipitation: String,
+    /*val perc_temperature: Int,
     val UV_Index: Int,
     val humidity: Int,
     val wind: Int,
     val coverage: Int,
-    val rain: Int
+    val rain: Int */
 )
