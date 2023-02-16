eb609a174358f8ef82156ea49bacf17e0927b398
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-02-16 12:11:32 +0100

    Update: update the data object and HomeScreen

--- app/src/main/java/co/develhope/meteoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/home/HomeFragment.kt
@@ -1,15 +1,31 @@
 package co.develhope.meteoapp.ui.home
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.Data
+import co.develhope.meteoapp.Data.cardInfo1
+import co.develhope.meteoapp.Data.cardInfo2
+import co.develhope.meteoapp.Data.cardInfo3
+import co.develhope.meteoapp.Data.cardInfo4
+import co.develhope.meteoapp.Data.cardInfo5
+import co.develhope.meteoapp.Weather
 import co.develhope.meteoapp.databinding.FragmentHomeBinding
+import java.time.OffsetDateTime
 
 class HomeFragment : Fragment() {
+    private lateinit var adapter: HomeFragmentAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var homeScreen: List<Data.HomeScreenParts>
 
-    private var _binding:FragmentHomeBinding ? = null
+
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,10 +40,38 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerViewHomeFrag.layoutManager = layoutManager
+        binding.recyclerViewHomeFrag.setHasFixedSize(true)
+        binding.recyclerViewHomeFrag.adapter = HomeFragmentAdapter(listDataHomeScreen)
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
+
+    private val title = Data.HomeTitle("Catania", "Sicilia")
+    private val nextDays = Data.Home5NextDays("Prossimi 5 giorni")
+
+    private val listDataHomeScreen = listOf<Data.HomeScreenParts>(
+        Data.HomeScreenParts.Title(title),
+        Data.HomeScreenParts.Card(cardInfo1),
+        Data.HomeScreenParts.Next5DaysString(nextDays),
+        Data.HomeScreenParts.Card(cardInfo2),
+        Data.HomeScreenParts.Card(cardInfo3),
+        Data.HomeScreenParts.Card(cardInfo4),
+        Data.HomeScreenParts.Card(cardInfo5)
+    )
+
+
 }
