afc885d07b6fa15f0a3185b97bd71176f7ebe0c1
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-03-01 15:48:59 +0100

    Update : changed from object to class

--- app/src/main/java/co/develhope/meteoapp/networking/OpenMeteoRetrofitInstance.kt
+++ app/src/main/java/co/develhope/meteoapp/networking/OpenMeteoRetrofitInstance.kt
@@ -3,8 +3,9 @@ package co.develhope.meteoapp.networking
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object OpenMeteoRetrofitInstance {
-    private fun retrofitGetData(){
+class OpenMeteoRetrofitInstance {
+
+    private fun retrofitGetData() {
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://api.open-meteo.com")
