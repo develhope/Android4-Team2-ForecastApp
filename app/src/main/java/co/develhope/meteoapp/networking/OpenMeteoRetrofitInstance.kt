d66c937b9a8f344aec63d60b0bfb3ec52353b664
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-03-01 15:58:33 +0100

    Update: interceptor added, maybe(?)

--- app/src/main/java/co/develhope/meteoapp/networking/OpenMeteoRetrofitInstance.kt
+++ app/src/main/java/co/develhope/meteoapp/networking/OpenMeteoRetrofitInstance.kt
@@ -1,11 +1,19 @@
 package co.develhope.meteoapp.networking
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class OpenMeteoRetrofitInstance {
 
     private fun retrofitGetData() {
+        val logging = HttpLoggingInterceptor()
+        val authorizationInterceptor = AuthorizationInterceptor()
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor(authorizationInterceptor)
+            .build()
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://api.open-meteo.com")
