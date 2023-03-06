42bd269a01861392a05a08e0b5b9bb146145c794
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-03-06 10:34:02 +0100

    Update: Retrofit instance for Meteo Api and network call for WeeklySummary

---
+++ app/src/main/java/co/develhope/meteoapp/networking/weeklySummary/WeeklySummaryInterface.kt
@@ -0,0 +1,26 @@
+package co.develhope.meteoapp.networking.weeklySummary
+
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeeklySummaryInterface {
+    @GET("/v1/forecast")
+    suspend fun getWeeklyData(
+        @Query("current_weather") currentWeather: Boolean?,
+        @Query("daily") daily: List<String> = listOf(
+            "weathercode",
+            "temperature_2m_max",
+            "temperature_2m_min",
+            "sunrise",
+            "sunset",
+            "precipitation_sum",
+            "rain_sum",
+            "windspeed_10m_max"
+        ),
+        @Query("latitude") latitude: Double?,
+        @Query("longitude") longitude: Double?,
+        @Query("timezone") timezone: String? = "Europe/Berlin"
+    ): WeatherResponse
+}
