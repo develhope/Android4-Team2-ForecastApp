72a98fb24ff4ec19117a9a99ed72dd35ada3b60c
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-03-13 16:59:56 +0100

    Update: Splash Screen added

---
+++ app/src/main/java/co/develhope/meteoapp/ui/SplashScreen.kt
@@ -0,0 +1,35 @@
+package co.develhope.meteoapp.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import co.develhope.meteoapp.MainActivity
+import co.develhope.meteoapp.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        val window = this.window
+        if (window != null) {
+            window.statusBarColor = this.getColor(R.color.bg_Home) ?: 0
+
+            window.decorView.setSystemUiVisibility(0)
+
+        }
+
+     Handler().postDelayed({
+         val intent = Intent(this,MainActivity::class.java)
+         fun test(){
+             Log.d("SplashScreen","Test for SplashScreen")
+         }
+         startActivity(intent)
+         finish()
+     },5000)
+    }
+}
