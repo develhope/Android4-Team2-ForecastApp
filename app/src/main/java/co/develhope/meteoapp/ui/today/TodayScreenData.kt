e17b852ba9eec13942f7dd2bfd660ec680a75e17
Jetzko <andreadefilippi68@gmail.com>
2023-03-15 17:47:21 +0100

    Update: Conflicts Fix

--- app/src/main/java/co/develhope/meteoapp/ui/today/TodayScreenData.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/today/TodayScreenData.kt
@@ -1,8 +1,8 @@
 package co.develhope.meteoapp.ui.today
 
-import co.develhope.meteoapp.networking.domainmodel.TodayCardData
+import co.develhope.meteoapp.networking.domainmodel.ForecastData
 
 sealed class TodayScreenData() {
     data class TodayScreenTitle(val todayTitle: TodayTitleData) : TodayScreenData()
-    data class TodayScreenCard(val todayCard : TodayCardData) : TodayScreenData()
+    data class TodayScreenCard(val todayCard : ForecastData) : TodayScreenData()
 }
